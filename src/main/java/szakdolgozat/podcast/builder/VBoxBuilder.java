ada4b139fd46358d05744a759e3da3714374885b
Toth Daniel <toth.daniel@icell.hu>
2015-11-05 16:29:43 +0100

    factory and builder pattern is almost done

--- src/main/java/szakdolgozat/podcast/builder/VBoxBuilder.java
+++ src/main/java/szakdolgozat/podcast/builder/VBoxBuilder.java
@@ -1,7 +1,10 @@
 package szakdolgozat.podcast.builder;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import szakdolgozat.podcast.gui.decorator.Decorator;
 
@@ -23,14 +26,33 @@ public class VBoxBuilder {
 
 	public static VBoxBuilder create() {
 		vbox = Decorator.decorateVBoxFactory(new VBox(Decorator.VBOXPADDING));
+		vbox.setAlignment(Pos.CENTER_LEFT);
 		return getInstance();
 	}
 
-	public VBoxBuilder info(String text) {
-		vbox.getChildren().add(Decorator.decorateTextFactory(
-				new Text(
-						text.length() > 20 ? new String(new StringBuilder(text.substring(0, 20)).append("...")) : text),
-				Decorator.SMALLTEXTSIZE));
+	public VBoxBuilder centerLeftAlignment() {
+		vbox.setAlignment(Pos.TOP_LEFT);
+		return getInstance();
+	}
+
+	public VBoxBuilder bigText(final String text) {
+		vbox.getChildren().add(Decorator.decorateTextFactory(new Text(text), Decorator.BIGTEXTSIZE));
+		return getInstance();
+	}
+
+	public VBoxBuilder smallText(final String text) {
+		vbox.getChildren().add(Decorator.decorateTextFactory(new Text(text), Decorator.SMALLTEXTSIZE));
+		return getInstance();
+	}
+
+	public VBoxBuilder noHBox(HBox hbox) {
+		vbox.getChildren().add(hbox);
+		return getInstance();
+	}
+
+	public VBoxBuilder bigRectangle(String image) {
+		vbox.getChildren().add(Decorator.decorateRectangleFactory(new Rectangle(), Decorator.BIGRECTANGLEHEIGHT,
+				Decorator.BIGRECTANGLEWIDTH, image));
 		return getInstance();
 	}
 
